3f90f50381d5f673d2fac34ca393a76854d7b4f0
Adenaan <adenaan@gmail.com>
2025-05-27 01:38:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const AUTHOR_NAME = 'Adenaan Jacobs'
 export const SITE_URL = 'https://ubuntucore.co.za'
-export const SITE_NAME = 'KD Propert Management'
+export const SITE_NAME = 'ubuntucore'
 export const SHOW_PER_PAGE = 10
