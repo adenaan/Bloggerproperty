6c9457ed8c7a946761e685ca0b0f58f402e0635c
Adenaan <adenaan@gmail.com>
2025-05-27 01:33:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const AUTHOR_NAME = 'Nuno Marques'
-export const SITE_URL = 'https://design-code.tips'
-export const SITE_NAME = 'design-code.tips'
+export const AUTHOR_NAME = 'Adenaan Jacobs'
+export const SITE_URL = 'https://ubuntucore.co.za'
+export const SITE_NAME = 'KD Propert Management'
 export const SHOW_PER_PAGE = 10
